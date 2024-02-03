18641187bdd966bfc4fb726c7a84b20f8918b429
javani999 <129900089+javani999@users.noreply.github.com>
2024-02-03 17:44:05 -0600

    Completed main code

--- JDS.HW2.UI/JDS.HW2.UI.cpp
+++ JDS.HW2.UI/JDS.HW2.UI.cpp
@@ -2,10 +2,44 @@
 //
 
 #include <iostream>
-
+#include <conio.h>
+#include <string>
+using namespace std;
 int main()
 {
+	float Add(float num1, float num2);
+	float Sub(float num1, float num2);
+	float Mul(float num1, float num2);
+	float Divide(float num1, float num2);
+	float pow(float num1, float num2);
+
+	
+	float num1, num2, answer;
+	char operation;
+
     std::cout << "Please input two numbers. After, input any of the shown characters for calculating: +, -, *, /, ^\n";
-	std::cin << 
+	std::cin >> num1;
+	std::cin >> num2;
+
+	switch (operation) {
+	case '+':
+		answer = Add(num1, num2);
+		cout << "Your answer is : " << answer << endl;
+		break;
+	case '-':
+		answer = Sub(num1, num2);
+		cout << "Your answer is : " << answer << endl; 
+		break;
+	case '*':
+		answer = Mul(num1, num2);
+		cout << "Your answer is : " << answer << endl;
+	case '^':
+		answer = pow(num1, num2);
+		cout << "Your answer is : " << answer << endl;
+		
+	}
+
+	_getch();
+	return 0;
 }
 
