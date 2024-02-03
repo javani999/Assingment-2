806c7680308d2c4b2f867d06b6b3b4053aa0a781
javani999 <129900089+javani999@users.noreply.github.com>
2024-02-03 17:16:44 -0600

    Add project files.

---
+++ JDS.HW2.UI/JDS.HW2.UI.cpp
@@ -0,0 +1,11 @@
+// JDS.HW2.UI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Please input two numbers. After, input any of the shown characters for calculating: +, -, *, /, ^\n";
+	std::cin << 
+}
+
